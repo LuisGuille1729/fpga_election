8f776e365f9346f374a99444e77d0c2a43e380da
ragoo <ragoo@mit.edu>
2024-11-11 19:29:05 -0500

    fsm adder checkpoint

--- fpga_e/hdl/pipeliner.sv
+++ fpga_e/hdl/pipeliner.sv
@@ -1,16 +1,16 @@
 `default_nettype none
 module pipeliner  #(
     parameter PIPELINE_STAGE_COUNT = 1,
-    parameter  DATA_BIT_SIZE= 0 )
+    parameter  DATA_BIT_SIZE= 1 )
     (
         input wire clk_in,
         input wire rst_in,
-        input wire[DATA_BIT_SIZE:0] data_in,
-        output logic[DATA_BIT_SIZE:0] data_out
+        input wire[DATA_BIT_SIZE-1:0] data_in,
+        output logic[DATA_BIT_SIZE-1:0] data_out
     );
 
 
-    logic [DATA_BIT_SIZE:0] data_pipe [PIPELINE_STAGE_COUNT-1:0];
+    logic [DATA_BIT_SIZE-1:0] data_pipe [PIPELINE_STAGE_COUNT-1:0];
     assign data_out = data_pipe[PIPELINE_STAGE_COUNT-1]; 
     always_ff @(posedge clk_in)begin
         if (rst_in) begin
