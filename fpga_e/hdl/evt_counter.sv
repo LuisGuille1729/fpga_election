88b5bd5ed1066871b246fda7381e428c263fb328
ragoo <ragoo@mit.edu>
2024-11-16 17:07:22 -0500

    top level checkpoint

--- fpga_e/hdl/evt_counter.sv
+++ fpga_e/hdl/evt_counter.sv
@@ -1,17 +1,20 @@
 `default_nettype none
-module evt_counter #(parameter MAX_COUNT = 1_000_000)
-  ( input wire          clk_in,
+module evt_counter #(parameter MAX_COUNT) (  // Slightly modified version of lab 2's event counter
+    input wire          clk_in,
     input wire          rst_in,
     input wire          evt_in,
-    output logic[15:0]  count_out
-  );
+    output logic[$clog2(MAX_COUNT) - 1: 0]  count_out
+    );
  
-  always_ff @(posedge clk_in) begin
-    if (rst_in || count_out == MAX_COUNT) begin
-      count_out <= 16'b0;
-    end else begin
-      count_out <= evt_in ? count_out + 1 : count_out;
+    always_ff @(posedge clk_in) begin
+        if (rst_in) begin
+            count_out <= 0;
+        end else if (evt_in) begin
+            if (count_out == MAX_COUNT - 1) count_out <= 0;
+            else count_out <= count_out + 1;
+        end else begin
+            count_out <= count_out;
+        end
     end
-  end
 endmodule
 `default_nettype wire
--- fpga_e/hdl/top_level.sv
+++ fpga_e/hdl/top_level.sv
@@ -10,9 +10,54 @@ module top_level
   logic   sys_rst;
   assign sys_rst = btn[0];
   assign start = btn[1];
+  localparam REGISTER_SIZE = 32;
+  localparam PRIME_SIZE = 1024;
+  localparam N_SIZE = 2*PRIME_SIZE;
+  localparam G_SIZE = N_SIZE;
+  localparam N_SQUARED_SIZE = 2*N_SIZE;
+  localparam R_SIZE = $clog2(N_SQUARED_SIZE) + 1;
+  localparam R_SQUARED_MOD_SIZE = N_SQUARED_SIZE;
+  localparam K_SIZE = N_SQUARED_SIZE;
+  localparam T_SIZE = 2*N_SQUARED_SIZE;
+
+
+  // for decryption
+  //localparam LAMBDA_SIZE = N_SIZE;
+  // localparam MU = N_SIZE
+
+
+localparam NUM_N_BLOCKS = N_SIZE/REGISTER_SIZE;
+localparam NUM_G_BLOCKS = G_SIZE/REGISTER_SIZE;
+localparam NUM_N_SQUARED_BLOCKS = N_SQUARED_SIZE/REGISTER_SIZE;
+localparam NUM_R_SQUARED_BLOCKS =  R_SQUARED_MOD_SIZE/REGISTER_SIZE;
+localparam NUM_K_BLOCKS = K_SIZE/REGISTER_SIZE;
+localparam NUM_T_BLOCKS = T_SIZE/REGISTER_SIZE;
+
+
+
+logic [$clog2(NUM_N_BLOCKS)-1:0][REGISTER_SIZE-1:0] n;
+logic [$clog2(NUM_G_BLOCKS)-1:0][REGISTER_SIZE-1:0] g; 
+logic [$clog2(NUM_N_SQUARED_BLOCKS)-1:0][REGISTER_SIZE-1:0] n_squared;
+ logic [$clog2(NUM_R_SQUARED_BLOCKS)-1:0][REGISTER_SIZE-1:0]r_squared; 
+ logic [$clog2(NUM_K_BLOCKS)-1:0][REGISTER_SIZE-1:0] k;
+
+
+logic [R_SIZE-1:0] r; 
+assign r = N_SQUARED_SIZE; 
+
+
+// We are assuming we are sending the bits in lsb first order
+
+
+
+
+
   
   logic valid_data;
   logic [7:0] data_received_byte;
+  
+
+
 
   uart_receive #(
     .INPUT_CLOCK_FREQ(100_000_000), // may change
@@ -42,20 +87,95 @@ vote_processor #(
   .vote_out(candidate_vote),
   .valid_vote_out(valid_vote_out)
 );
-
-logic [31:0] random_block;
+logic [REGISTER_SIZE-1:0] random_block;
 logic random_valid;
 
+// generates a 4096 bit output in register size sizes, but the topmost 2048 bits are 0
 rand_gen#(
-  .BITSIZE(32)
+  .BITSIZE(REGISTER_SIZE)
 ) 
 rng
 (
   .clk_in(clk_100mhz),
   .rst_in(sys_rst),
+  .trigger_in(valid_vote_out),
   .rand_out(random_block),
   .valid_out(random_valid)
 );
+
+logic[$clog2(NUM_R_SQUARED_BLOCKS)-1:0] r_squared_select_index;
+logic[REGISTER_SIZE-1:0] r_squared_mod_select_out;
+assign r_squared_mod_select_out = r_squared[r_squared_select_index];
+evt_counter #(.MAX_COUNT(NUM_R_SQUARED_BLOCKS))
+  ( .clk_in(clk_100mhz),
+    .rst_in(sys_rst),
+    .evt_in(random_valid),
+    .count_out(r_squared_mod_select_out)
+  );
+
+
+logic [REGISTER_SIZE-1:0] placeholder1_mult_out;
+logic placeholder1_mult_valid_out;
+fsm_multiplier  #(
+    .register_size(REGISTER_SIZE),
+    .bits_in_num(R_SQUARED_MOD_SIZE)
+    )
+entry_multplier
+    (
+        .n_in(random_block),
+        .m_in(r_squared_mod_select_out),
+        .valid_in(random_valid),
+        .rst_in(sys_rst),
+        .clk_in(clk_100mhz),
+        .data_out(placeholder1_mult_out),
+        .valid_out(placeholder1_mult_valid_out),
+    );
+
+
+logic[$clog2(NUM_N_SQUARED_BLOCKS)-1:0] n_squared_select_index;
+logic[REGISTER_SIZE-1:0] n_squared_select_out;
+assign n_squared_select_out = n_squared[n_squared_select_index];
+
+logic[$clog2(NUM_K_BLOCKS)-1:0] k_select_index;
+logic[REGISTER_SIZE-1:0] k_select_out;
+assign k_select_out = k[k_select_index];
+
+logic consumed_n_squared_out;
+
+evt_counter #(.MAX_COUNT(NUM_N_SQUARED_BLOCKS))
+  ( .clk_in(clk_100mhz),
+    .rst_in(sys_rst),
+    .evt_in(consumed_n_squared_out),
+    .count_out(n_squared_select_out)
+  );
+  evt_counter #(.MAX_COUNT(NUM_K_BLOCKS))
+  ( .clk_in(clk_100mhz),
+    .rst_in(sys_rst),
+    .evt_in(placeholder1_mult_valid_out),
+    .count_out(k_select_out)
+  );
+
+
+logic [REGISTER_SIZE-1:0] reduced_product_block;
+logic placeholder_reduce_valid1;
+  montgomery_reduce#(
+    .register_size(REGISTER_SIZE),
+    .num_blocks(NUM_T_BLOCKS),
+  ) reducer1(
+    .clk_in(clk_100mhz),
+    .rst_in(sys_rst),
+    .valid_in(placeholder1_mult_valid_out),
+    .product_t_in(placeholder1_mult_out),
+    .k_in(k_select_out),
+    .n_squared_in(n_squared_select_out),
+    .consumed_n_squared_out(consumed_n_squared_out),
+    .data_out(reduced_product_block),
+    .valid_out(placeholder_reduce_valid1)
+)
+    
+
+
+    
 
 
 
