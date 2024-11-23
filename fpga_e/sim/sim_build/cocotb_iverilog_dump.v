329e35c5976b1492d9727663df1aa742044ede52
Luis Turino <turino14@mit.edu>
2024-11-22 23:38:16 -0500

    Modify gitignore

--- fpga_e/sim/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("/Users/turino14/6205/secure_election/fpga_e/sim/sim_build/right_shifter.fst");
-    $dumpvars(0, right_shifter);
-end
-endmodule
