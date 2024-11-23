383a2a2bdcdeb27a52c00e71d414db0be2f6e44f
Luis Turino <turino14@mit.edu>
2024-11-23 14:52:18 -0500

    Implement and test zero-cycle comparator

--- fpga_e/hdl/running_comparator.sv
+++ fpga_e/hdl/running_comparator.sv
@@ -21,75 +21,60 @@ module running_comparator #(
     output logic [1:0] comparison_result_out
 );
 
-    enum bit [1:0] {IDLE, A_LESS_THAN_B, A_EQUAL_B, A_GREATER_THAN_B} state;
     // Note: See lucid-chart for FSM diagram
     // https://lucid.app/lucidchart/f2744b53-02a1-47fa-b0ea-dd6aaf4cb5ec/edit?invitationId=inv_78c14da0-c287-4d73-a1f3-bd751441b047&page=KcXtH5rtSSBn#
-
-    assign comparison_result_out = state;
+    enum bit [1:0] {IDLE, A_LESS_THAN_B, A_GREATER_THAN_B, A_EQUAL_B} state;
 
     logic [$clog2(NUM_BLOCKS):0] block_count;
 
+    always_comb begin : determine_result
+       case (state)
+            IDLE: begin
+                if (!valid_in) 
+                    comparison_result_out = IDLE;
+                else if (block_numA_in == block_numB_in)
+                    comparison_result_out = A_EQUAL_B;
+                else if (block_numA_in < block_numB_in)
+                    comparison_result_out = A_LESS_THAN_B;
+                else
+                    comparison_result_out = A_GREATER_THAN_B;
+            end
+
+            A_LESS_THAN_B: begin
+                if (block_numA_in > block_numB_in)
+                    comparison_result_out = A_GREATER_THAN_B;
+                else 
+                    comparison_result_out = A_LESS_THAN_B;
+            end
+
+            A_EQUAL_B: begin
+                if (block_numA_in < block_numB_in)
+                    comparison_result_out = A_LESS_THAN_B;
+                else if (block_numA_in > block_numB_in)
+                    comparison_result_out = A_GREATER_THAN_B;
+                else
+                    comparison_result_out = A_EQUAL_B;
+            end
+
+            A_GREATER_THAN_B: begin
+                if (block_numA_in < block_numB_in)
+                    comparison_result_out = A_LESS_THAN_B;
+                else
+                    comparison_result_out = A_GREATER_THAN_B;
+            end
+       endcase
+    end
+
     always_ff @( posedge clk_in ) begin
-        if (rst_in) begin
+        if (rst_in || block_count == NUM_BLOCKS-1) begin
             state <= IDLE;
             block_count <= 0;
-        end
-        else if (block_count == NUM_BLOCKS) begin
-            // if valid, do first comparison
-            if (valid_in) begin
-                block_count <= 1;
-                if (block_numA_in == block_numB_in)
-                        state <= A_EQUAL_B;
-                    else if (block_numA_in < block_numB_in)
-                        state <= A_LESS_THAN_B;
-                    else
-                        state <= A_GREATER_THAN_B;
-            end
-            else begin // else go to idle
-                state <= IDLE;
-                block_count <= 0;
-            end
-
         end else if (valid_in) begin 
+            state <= comparison_result_out; // update state
             block_count <= block_count + 1;
-
-            case (state)
-                IDLE: begin
-                    if (block_numA_in == block_numB_in)
-                        state <= A_EQUAL_B;
-                    else if (block_numA_in < block_numB_in)
-                        state <= A_LESS_THAN_B;
-                    else
-                        state <= A_GREATER_THAN_B;
-                end
-
-                A_LESS_THAN_B: begin
-                    if (block_numA_in > block_numB_in)
-                        state <= A_GREATER_THAN_B;
-                    // else A_LESS_THAN_B
-                end
-
-                A_EQUAL_B: begin
-                    if (block_numA_in < block_numB_in)
-                        state <= A_LESS_THAN_B;
-                    else if (block_numA_in > block_numB_in)
-                        state <= A_GREATER_THAN_B;
-                    // else A_EQUAL_B
-                end
-
-                A_GREATER_THAN_B: begin
-                    if (block_numA_in < block_numB_in)
-                        state <= A_LESS_THAN_B;
-                    // else A_GREATER_THAN_B
-                end
-
-
-
-            endcase
         end
 
     end
-
 
 endmodule
 
--- fpga_e/sim/test_running_comparator.py
+++ fpga_e/sim/test_running_comparator.py
@@ -23,6 +23,27 @@ async def delay(dut, time, addRandomDelay=False):
     if addRandomDelay:
         await ClockCycles(dut.clk_in, random.randint(0, 1))
  
+async def comparison_test(dut, A, B):
+    print(f"Compare: A is {A.bit_length()}-bits, B is {B.bit_length()}-bits.")
+    if (A == B):
+        expected = 0b11
+    elif (A < B):
+        expected = 0b01
+    else:
+        expected = 0b10
+    
+    
+    for count in range(128):
+        A_in = A & (0xFFFF_FFFF)
+        B_in = B & (0xFFFF_FFFF)
+        await send(dut, A_in, B_in)
+        A = A >> 32
+        B = B >> 32
+        
+        assert dut.block_count.value == count    
+    
+    assert int(dut.comparison_result_out.value) == expected
+ 
 @cocotb.test()
 async def first_test(dut):
     """ First cocotb test?"""
@@ -54,70 +75,75 @@ async def first_test(dut):
         A = A >> 32
         # B = B >> 32
         
-        if count == 0:
-            assert dut.comparison_result_out.value == 0
-        if count != 0:
-            assert dut.comparison_result_out.value == 2
+        assert dut.comparison_result_out.value == 0b11
         
         print(count)
         assert dut.block_count.value == count    
         count += 1
     
     await delay(dut, 1, addRandomDelay=False)
-    assert dut.comparison_result_out.value == 2
-    await delay(dut, 1, addRandomDelay=False)
     assert dut.comparison_result_out.value == 0
+    await delay(dut, 10, addRandomDelay=False) 
     
-    # Send twice immediately
-    
-    A = random.randint(2**4095, 2**4096-1)
-    # B = A
-    print(f"A = B. A is {A.bit_length()}-bits, B is {A.bit_length()}-bits.")
+    #### Test A LESS THAN B ####
+    B = random.randint(2**4095, 2**4096-1)
+    A = B - random.randint(0, 2**4094)
+    print(f"A < B. A is {A.bit_length()}-bits, B is {B.bit_length()}-bits.")
     
     count = 0
     while A != 0:
         A_in = A & (0xFFFF_FFFF)
-        await send(dut, A_in, A_in)
+        B_in = B & (0xFFFF_FFFF)
+        await send(dut, A_in, B_in)
         A = A >> 32
-        # B = B >> 32
+        B = B >> 32
         
-        if count == 0:
-            assert dut.comparison_result_out.value == 0
-        if count != 0:
-            assert dut.comparison_result_out.value == 2
         
         print(count)
         assert dut.block_count.value == count    
         count += 1
     
+    assert dut.comparison_result_out.value == 0b01
+    await delay(dut, 1, addRandomDelay=False)
+    assert dut.comparison_result_out.value == 0
+    await delay(dut, 10, addRandomDelay=False) 
     
+    #### Test A GREATER THAN B ####
     A = random.randint(2**4095, 2**4096-1)
-    # B = A
-    print(f"A = B. A is {A.bit_length()}-bits, B is {A.bit_length()}-bits.")
+    B = A - random.randint(0, 2**4094)
+    print(f"A > B. A is {A.bit_length()}-bits, B is {B.bit_length()}-bits.")
     
     count = 0
     while A != 0:
         A_in = A & (0xFFFF_FFFF)
-        await send(dut, A_in, A_in)
+        B_in = B & (0xFFFF_FFFF)
+        await send(dut, A_in, B_in)
         A = A >> 32
-        # B = B >> 32
+        B = B >> 32
         
-        if count == 0:
-            assert dut.comparison_result_out.value == 2 # receiving equal from previous comparison
-        if count != 0:
-            assert dut.comparison_result_out.value == 2
         
-        print(count, dut.block_count.value)
-        if count == 0:  
-            assert dut.block_count.value == 0x80
-        else:
-            assert dut.block_count.value == count
+        print(count)
+        assert dut.block_count.value == count    
         count += 1
     
-    await delay(dut, 1, addRandomDelay=False)
-    assert dut.comparison_result_out.value == 2
+    assert dut.comparison_result_out.value == 0b10
     await delay(dut, 1, addRandomDelay=False)
     assert dut.comparison_result_out.value == 0
+    await delay(dut, 10, addRandomDelay=False) 
+    
+    
+    ### STRESS TEST ###
+    print("Stress test:")
+    for _ in range(100):
+        A = random.randint(0, 2**4096-1)
+        B = random.randint(0, 2**4096-1)
+        await comparison_test(dut, A, B)
+    
+    for _ in range(100):
+        # test with very small differences
+        A = random.randint(0, 2**4096-1)
+        B = A + random.choice([0, 2**100, -(2**100), 1, -1])
+        await comparison_test(dut, A, B)
     
  
 """the code below should largely remain unchanged in structure, though the specific files and things
